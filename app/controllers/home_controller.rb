c21367f10c270e1aa7facf245b636180d0c87ef4
Rogelio Alvarado <rogalvil@gmail.com>
2017-12-03 18:36:23 -0600

    SLM - Include validators

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  before_action :set_opponents, only: [:log, :log_create]
+  
+
   def index
   end
 
@@ -9,10 +12,29 @@ class HomeController < ApplicationController
     @match = Match.new
     @match.scores.build(user: current_user, points:0)
     @match.scores.build(points:0)
-    @opponents = User.not_me(current_user.id)
+    
   end
 
   def log_create
-    
+    @match = current_user.matches.new(params_match)
+    if @match.save
+      flash[:notice] = "Log game successfully created"
+      redirect_to root_path
+    else
+      render 'log'
+    end
   end
+
+  private
+    def params_match
+      params.require(:match).permit(:played_at, scores_attributes: [:user_id, :points] )
+    end
+
+    def set_match
+      @match = Match.find(params[:id]) rescue nil
+    end
+
+    def set_opponents
+      @opponents = User.not_me(current_user.id)
+    end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,29 @@ class Match < ActiveRecord::Base
   has_many :users, :through => :scores
 
   accepts_nested_attributes_for :scores
+
+  validates :played_at, presence: true
+  validate :scores_limit_valid
+  validate :scores_over_twenty_one_valid
+  validate :scores_diference_two_points_valid
+
+  private 
+    def scores_limit_valid
+      unless scores.length == 2
+        errors.add(:scores, "they must be only 2")
+      end
+    end
+
+    def scores_over_twenty_one_valid
+      unless scores.map { |s| s.points if s.points >= 21 }.compact.any?
+        errors.add(:scores, "at least some must be greater than 21")
+      end
+    end
+
+    def scores_diference_two_points_valid
+      points = scores.map(&:points).sort.reverse
+      unless (points.first - points.last) >= 2
+        errors.add(:scores, "must be at least 2 points")
+      end
+    end
 end
